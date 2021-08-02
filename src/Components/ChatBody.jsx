e2db53b673dbb847a6033caa14c9d156c72001fd
Nicole Tevez <71675833+naniitevez@users.noreply.github.com>
2021-08-01 22:34:25 -0300

    acomodo codigo

--- src/Components/ChatBody.jsx
+++ src/Components/ChatBody.jsx
@@ -1,22 +1,22 @@
-import ChatBox from './ChatBox'
-import ChatCircleButton from './ChatCircleButton'
-import { ChatProvider } from '../Contexts/ChatContext';
-import { useState } from 'react'
+import ChatBox from "./ChatBox";
+import ChatCircleButton from "./ChatCircleButton";
+import { ChatProvider } from "../Contexts/ChatContext";
+import { useState } from "react";
 
 const ChatBody = () => {
+  const [showBox, setShowBox] = useState(false);
 
-    const [showBox, setShowBox] = useState(false);
+  return (
+    <ChatProvider>
 
-    return (
-
-        <ChatProvider>
-            {!showBox && <ChatCircleButton Click={() => setShowBox(true)} />}
-
-            {showBox && <ChatBox Close={() => setShowBox(false)} />}
-        </ChatProvider>
-
-    )
-}
-
+      {!showBox ? (
+        <ChatCircleButton Click={() => setShowBox(true)} />
+      ) : (
+        <ChatBox Close={() => setShowBox(false)} />
+      )}
+      
+    </ChatProvider>
+  );
+};
 
 export default ChatBody;
