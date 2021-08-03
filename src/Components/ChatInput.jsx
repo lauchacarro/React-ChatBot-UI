76c9be393286777a0f1a99022582d9dd9a0059c7
Laucha Carro <lautaroecarro@gmail.com>
2021-08-03 00:18:35 -0300

    Agrego form para que se envie el mensaje con el enter

--- src/Components/ChatInput.jsx
+++ src/Components/ChatInput.jsx
@@ -1,23 +1,31 @@
-import { useState } from 'react';
-import classes from './ChatInput.module.css'
+import { useState } from "react";
+import classes from "./ChatInput.module.css";
 
 const ChatInput = ({ SendMessage }) => {
+  const [message, setMessage] = useState();
 
-    const [message, setMessage] = useState();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    SendMessage(message);
+    setMessage("");
+  };
 
-    const handleButtonOnClick = () => {
-        SendMessage(message);
-        setMessage("")
-    }
-
-    return (
-        <div>
-            <input type="text" className={classes.input} placeholder="Send a message..." value={message} onChange={e => setMessage(e.target.value)} />
-            <button type="submit" className={classes.submit} onClick={handleButtonOnClick}><i className="material-icons">send</i></button>
-
-        </div>
-    )
-}
-
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className={classes.form}>
+        <input
+          type="text"
+          className={classes.input}
+          placeholder="Hazme una pregunta..."
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button type="submit" className={classes.submit}>
+          <i className="material-icons">send</i>
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default ChatInput;
